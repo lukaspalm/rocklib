7e0eaa76011adabbfc1fc412e554acc53495c666
LukasPalm <86203896+LukasPalm@users.noreply.github.com>
2024-03-23 14:59:31 +0100

    added pin on/off

--- rocklib.py
+++ rocklib.py
@@ -26,6 +26,52 @@ class camera:
         else:
             print("\nError: %s could not be found. \nMake sure the camera overlay is enabled and the camera is connected." % device)
 
+class pins:
+    def parsePinout(pin, mapping):
+        letters = "ABCD"
+        pinInfo = mapping[pin]
+        print("Pin info: %s" % pinInfo)
+        if isinstance(pinInfo, list):
+            return int(str(pinInfo[0][len(pinInfo[0])-1])) * 32 + int(letters.index(pinInfo[1][0])) * 8 + int(pinInfo[1][1])
+        
+        else:
+            return str(pinInfo)
+
+    
+    def initialize(pinout):
+        pins.pinout = pinout
+    
+    
+    def setPin(pin, state):
+        try: 
+            toggle = int(pins.parsePinout(pin, pins.pinout))
+            print("toggle is %s" % toggle)
+
+            if state < 0 or state > 1: 
+                raise ValueError
+            
+            if isinstance(toggle, int):
+                try:
+                    import os
+                    
+                    with open("/sys/class/gpio/export", "w") as f:
+                        f.write(str(toggle))
+                        f.close()
+
+                    with open(f"/sys/class/gpio/gpio{toggle}/direction", "w") as f:
+                        f.write("out")
+                        f.close()
+
+                    with open(f"/sys/class/gpio/gpio{toggle}/value", "w") as f:
+                        f.write(str(state))
+                        f.close()
+                except Exception as e:
+                    print("Error: %s" % e)
+            else: 
+                print("Error: %s not possible to toggle" % pins.pinout[pin])
+        except ValueError:
+            print("Error toggling pin %s.\nMake sure that the pin is not GND, 5V or 3.3v.\nAlso, make sure that the toggle value is 0 or 1." % pin)
+        
 
               
 
---
+++ testingpins.py
@@ -0,0 +1,8 @@
+from pinouts import pinouts
+from rocklib import pins
+
+pins.initialize(pinouts.ROCK4)
+
+pins.setPin(22, 1)
+
+
